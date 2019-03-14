741d8e9ec902d87671fd78b5c77267adb4c51833
RomanKovalitskiy <romaxakoval@gmail.com>
2019-03-14 20:41:42 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	preferences.unshift(0);
+	var count=0;
+	for (i=1; i<preferences.length;i++) {
+		var in2=preferences[i];
+		if (preferences[i]===i && preferences[in2]===i) {
+			continue;
+		};
+		var in3=preferences[in2];
+		var in1=preferences[in3];
+		if (isNaN(preferences[i]) && isNaN(preferences[in2]) && isNaN(preferences[in3])) {
+			continue;
+		};
+		if (i===in1) {
+			delete preferences[i];
+			delete preferences[in2];
+			delete preferences[in3];
+			++count;
+		};
+	}
+	return count;
 };
